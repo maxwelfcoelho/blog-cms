e9a25c0e0279a1de261eaee4d1c297ab9bbf5c5a
Maxwel <maxwelfcoelho@gmail.com>
2022-07-28 19:41:10 -0300

    return validation errors

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,8 +1,17 @@
+const { validationResult } = require('express-validator');
+
 const userService = require("../service/user.service");
 
 class UserController {
     async register(req, res) {
         const { email, password } = req.body;
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ 
+                errors: errors.array().map(error => error.msg)
+            });
+        }
 
         try {
             await userService.create({

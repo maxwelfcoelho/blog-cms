01aa81c5e282fad6203a74d5d8f802a5a1c4507f
Maxwel <maxwelfcoelho@gmail.com>
2022-07-28 19:40:22 -0300

    add validations to the user registration

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
+const { body } = require('express-validator');
 
 const userController = require("../controllers/user.controller");
 
 const router = Router();
 
-router.post("/api/v1/register", userController.register);
+router.post(
+    "/api/v1/register", 
+    body('email').isEmail().withMessage("Not a valid email"),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage("Password should be at least 8 characters"),
+    userController.register
+);
 
 module.exports = router;

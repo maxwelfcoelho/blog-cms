06305046eed45212edb9c560dc77e2c6f0ccad95
Maxwel <maxwelfcoelho@gmail.com>
2022-07-29 09:46:03 -0300

    login route

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -6,12 +6,21 @@ const userController = require("../controllers/user.controller");
 const router = Router();
 
 router.post(
-    "/api/v1/register", 
+    "/api/v1/register",
     body('email').isEmail().withMessage("Not a valid email"),
     body('password')
         .isLength({ min: 8 })
         .withMessage("Password should be at least 8 characters"),
     userController.register
+);
+
+router.post(
+    "/api/v1/login",
+    body('email').isEmail().withMessage("Not a valid email"),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage("Password should be at least 8 characters"),
+    userController.login
 );
 
 module.exports = router;

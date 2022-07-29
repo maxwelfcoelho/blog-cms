d6472b0a39ae8747dbbd8fc0bf9baf1da4e0f43c
Maxwel <maxwelfcoelho@gmail.com>
2022-07-29 11:59:26 -0300

    add create post validations

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 const postService = require("../service/post.service");
 
 class PostController {
@@ -5,6 +7,20 @@ class PostController {
         const posts = await postService.findAllPosts();
 
         res.status(200).json(posts);
+    }
+
+    async createPost(req, res) {
+        const { title, content } = req.body;
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ 
+                errors: errors.array().map(error => error.msg)
+            });
+        }
+
+
+        res.status(201).json();
     }
 }
 
--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,9 +1,29 @@
 const { Router } = require("express");
+const { body } = require("express-validator");
 
 const router = Router();
 
 const postController = require("../controllers/post.controller");
 
 router.get("/api/v1/posts", postController.findAllPosts);
+
+router.post(
+    "/api/v1/posts/new", 
+    body("title")
+        .notEmpty()
+        .withMessage("Title is required")
+        .isLength({ min: 20 })
+        .withMessage("Title should be at least length 20")
+        .isLength({ max: 150 })
+        .withMessage("Title should be at most length 150"),
+    body("content")
+        .notEmpty()
+        .withMessage("Content is required")
+        .isLength({ min: 50 })
+        .withMessage("Title should be at least length 50")
+        .isLength({ max: 255 })
+        .withMessage("Title should be at most length 255"),
+    postController.createPost
+);
 
 module.exports = router;

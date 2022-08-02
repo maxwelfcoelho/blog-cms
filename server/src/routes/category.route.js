e5731bb0706fe6b84213b3aa0c49c02e8309dc38
Maxwel <maxwelfcoelho@gmail.com>
2022-08-02 08:42:03 -0300

    create category route

---
+++ server/src/routes/category.route.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+
+const categoryController = require("../controllers/category.controller");
+
+const router = Router();
+
+router.get("/api/v1/categories", categoryController.findAllCategories);
